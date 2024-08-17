7fe09fb8af2af203480d7a5fa1fbefa545a0994b
조희수 <hs.jo.sw.p@gmail.com>
2024-08-17 15:42:29 +0900

    [조희수] 플레이어 애니메이션

--- Assets/02.Scripts/CharacterMoveAbility.cs
+++ Assets/02.Scripts/CharacterMoveAbility.cs
@@ -7,10 +7,12 @@ public class CharacterMoveAbility : MonoBehaviour
     public float moveSpeed = 5f;
     public VariableJoystick variableJoystick;
     private Rigidbody rb;
+    Animator _animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
     void Update()
@@ -19,6 +21,13 @@ public class CharacterMoveAbility : MonoBehaviour
         float moveZ = variableJoystick.Vertical;
 
         Vector3 move = transform.right * moveX + transform.forward * moveZ;
+
+        float speedValue = move.magnitude > 0 ? 1f : 0f;
+
+        _animator.SetFloat("Horizontal", Mathf.Lerp(_animator.GetFloat("Horizontal"), moveX, Time.deltaTime * 8));
+        _animator.SetFloat("Vertical", Mathf.Lerp(_animator.GetFloat("Vertical"), moveZ, Time.deltaTime * 8));
+        _animator.SetFloat("Speed", Mathf.Lerp(_animator.GetFloat("Speed"), speedValue, Time.deltaTime * 8));
+
         rb.MovePosition(transform.position + move * moveSpeed * Time.deltaTime);
     }
 }
