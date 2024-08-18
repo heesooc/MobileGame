276feb498ea2ba3c60f1b6f79d67bb643410e049
조희수 <hs.jo.sw.p@gmail.com>
2024-08-19 04:39:07 +0900

    [조희수] 플레이어 스택 애니메이션

--- Assets/02.Scripts/PlayerInteractionAbility.cs
+++ Assets/02.Scripts/PlayerInteractionAbility.cs
@@ -6,6 +6,12 @@ public class PlayerInteractionAbility : MonoBehaviour
 {
     public Transform handPosition; 
     private List<GameObject> holdBreads = new List<GameObject>();
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponentInChildren<Animator>(); 
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -20,6 +26,7 @@ public class PlayerInteractionAbility : MonoBehaviour
                     bread.transform.SetParent(handPosition);
                     bread.transform.localPosition = Vector3.up * holdBreads.Count * 0.2f; 
                     holdBreads.Add(bread);
+                    _animator.SetBool("IsHold", true);
                 }
             }
         }
